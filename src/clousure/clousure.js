2d365cc7d17d4457a31d3040f0d6681282d4aed5
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-18 17:39:22 -0600

    Creamos nuestro primer clousure

---
+++ src/clousure/clousure.js
@@ -0,0 +1,21 @@
+// esta funsion no recuerda los valores que ya le fueron asignados, por lo tengo saveCoins siempre se reinicia a 0
+
+const moneyBox = (coins) => {
+  var saveCoins = 0;
+  saveCoins += coins;
+  console.log(`MoneyBox : $${saveCoins}`);
+};
+
+moneyBox(5);
+moneyBox(10);
+
+// esta operacion en cambio tiene un clousure, o sea una forma de recordar los valores que ya le habian sido asignados.
+
+const moneyBox = () => {
+  var saveCoins = 0;
+  const countCoins = (coins) => {
+    saveCoins += coins;
+    console.log(`MoneyBox : $${saveCoins}`);
+  };
+  return countCoins;
+};
