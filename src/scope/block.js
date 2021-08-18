21aca8cafe238736973a798a6af922dc2d87ffb1
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-18 17:06:30 -0600

    entenemos el scope de bloque

---
+++ src/scope/block.js
@@ -0,0 +1,12 @@
+const fruits = () => {
+  if (true) {
+    var fruits1 = "Apple";
+    let fruits2 = "Banana";
+    const fruits3 = "Kiwi";
+  }
+  console.log(fruits1);
+  console.log(fruits2);
+  console.log(fruits3);
+};
+
+fruits();
