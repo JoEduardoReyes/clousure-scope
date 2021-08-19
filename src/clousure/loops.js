f8a8956a02c5d42862f2f6eeb247ee58c82b378a
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-19 12:23:58 -0600

    Loops y clousures

---
+++ src/clousure/loops.js
@@ -0,0 +1,11 @@
+// al usar var creamos de forma involuntaria un clousure que no cumple lo que queremos.
+
+const anotherFunction = () => {
+  for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, 1000);
+  }
+};
+
+anotherFunction();
--- src/clousure/private.js
+++ src/clousure/private.js
@@ -1,3 +1,5 @@
+// A los clousures les podemos agregar metodos para asignar o quitar ciertos valores. De esta forma retornamos una variable privada.
+
 const person = () => {
   var saveName = "Name";
   return {
