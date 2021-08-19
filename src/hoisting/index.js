8d0221da3c38aa1ee7d8ee14c94d6152894710b2
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-19 12:44:10 -0600

    Que es el hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,15 @@
+// el hosting o eleveascion, Javascript eleva todas las vriables y funciones para ser declaradas y guardadas en momeria.
+
+a = 2;
+var a;
+
+console.log(a);
+
+console.log(a);
+var a = 2;
+
+function nameOfDog(name) {
+  console.log(name);
+}
+
+nameOfDog("Elmo");
