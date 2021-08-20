ee5836007288fd7bc8b938dac230ea2afbd052bc
Eduardo Reyes <joeduardoreyes@outlook.com>
2021-08-20 10:17:23 -0600

    Terminamos el curso

---
+++ src/debug/index.js
@@ -0,0 +1,34 @@
+var a = "Hello";
+
+function hello() {
+  let b = "Hello World";
+  const c = "Hello World!";
+  if (true) {
+    let d = "Hello World!!";
+    debugger;
+  }
+}
+
+hello();
+
+// money box
+
+const moneyBox = () => {
+  var saveCoins = 0;
+  const countCoins = (coins) => {
+    saveCoins += coins;
+    console.log(`MoneyBox : $${saveCoins}`);
+  };
+  return countCoins;
+};
+
+let myMoneyBox = moneyBox();
+
+myMoneyBox(4);
+myMoneyBox(6);
+myMoneyBox(7);
+
+const helloWorld = () => {
+  globalVar = "I'm global";
+};
+helloWorld();
